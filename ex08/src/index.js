b6e2d0d8d7f3ab960d119dd9e53664613f36f577
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 21:57:59 +0200

    Add Exercise 08

---
+++ ex08/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var sum = numOne + numTwo; 
+    // Only change code above this line
+   return sum;
+
+}
+    
+console.log(main(150, 425));   
+module.exports = main;
