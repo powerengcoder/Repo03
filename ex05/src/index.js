7d64d28fe3e630952cf00745dd6864fec86eaa31
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 20:40:32 +0200

    Add Exercise 04

---
+++ ex05/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //only change code below this line
+    var fooBoo = 0;
+    var x = 8;
+    // only change code above this line
+    return x;
+
+}
+    
+console.log(main());
+    
+module.exports = main;
