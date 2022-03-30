e971cc4fce80859299a30399d1fefe5b980b198c
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 19:43:22 +0200

    Add Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //only change code below this line
+    //This is a single line JavaScript comment
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+     */
+    // only change code above this line
+    
+    }
+    
+    main();
+    
+    module.exports =main;
