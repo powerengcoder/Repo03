26f6080faebb83a31f63e5b8ab454b0eda63e728
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 22:04:23 +0200

    Add Exercise 09

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var difference = numOne - numTwo; 
+    // Only change code above this line
+   return difference;
+
+}
+    
+console.log(main(425, 120));   
+module.exports = main;
