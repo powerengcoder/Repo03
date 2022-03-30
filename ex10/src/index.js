9025544ab192f4709eaa070340fdfc57b2f30b4c
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 22:08:24 +0200

    Add Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var product = numOne * numTwo; 
+    // Only change code above this line
+   return product;
+
+}
+    
+console.log(main(25, 8));   
+module.exports = main;
