f1ab49fa6a7d26b5421f4d217ede0cd40cc3b1a8
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-03-30 19:53:56 +0200

    Add Exercise 03

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //only change code below this line
+    var forCat;
+    // only change code above this line
+    
+    }
+    
+    console.log(main());
+    
+    module.exports =main;
